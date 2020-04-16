067cc24a00664ba1a0d5eb7664fdf36faec5bdfe
Erick Rodriguez <erickrod54@gmail.com>
2020-04-16 01:35:49 -0400

    context-vs-scope

---
+++ Advanced JavaScript Concepts/Seccion 3/Clase 50/context-vs-scope.js
@@ -0,0 +1,27 @@
+/**
+ *                  CONTEXT VS SCOPE
+ * (DOS TERMINOS DISTINTOS -ENFATIZAR SUS DIFERENCIAS)
+ * 
+ * 
+ * context - runtime (relacion directa al execution context) 
+ *                   (relacion directa al dinamyc scope 
+ *                   "desde donde se estan llamando '()' las 
+ *                   funciones en nuestro codigo determinara el 
+ *                   acceso a funciones, variables y data")
+ *                 
+ * Es mas basado en los objetos, esta mas frecuentemente
+ * determinado en, como una variable determina el valor 
+ * del keyword "this" que es una referencia al "objeto" que se 
+ * ejecuta en el momento
+ * 
+ * 
+ * scope - lexical (relacion directa al lexical enviroment)
+ *                 (relacion directa al lexical scope, donde 
+ *                 el codigo es escrito determina acceso a 
+ *                 funciones, variables y data)
+ * 
+ * se refiere a la visibilidad (acceso a variables)- 
+ * basado en las funciones, funcionando con los variable
+ * enviroment
+ * 
+ */
