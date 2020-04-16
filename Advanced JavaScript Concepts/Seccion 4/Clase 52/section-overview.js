84c837d510bf939ee6f954a9c0145550b3f4d44f
Erick Rodriguez <erickrod54@gmail.com>
2020-04-16 17:37:12 -0400

    section brief-revisar al terminarla

---
+++ Advanced JavaScript Concepts/Seccion 4/Clase 52/section-overview.js
@@ -0,0 +1,8 @@
+/**
+ * --------------TYPES IN JAVASCRIPT----------------
+ * 
+ * esta seccion es la base para la seccion siguiente 
+ * "los 2 pilares de JavaScript" veremos como todos
+ * cada punto de esta seccion se relaciona con los tipos
+ * de datos
+ */
